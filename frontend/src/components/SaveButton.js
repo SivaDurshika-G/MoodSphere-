3ae9cc6d1e20016b155c482a356c0d13243f98ef
vinodravi84 <vinodravi12b2svv@gmail.com>
2025-08-07 17:16:12 +0530

    Save Button Configured

--- frontend/src/components/SaveButton.js
+++ frontend/src/components/SaveButton.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SaveButton({ onClick }) {
+  const [isSaved, setIsSaved] = useState(false);
+
+  const handleClick = () => {
+    setIsSaved(true);
+    onClick(); // Call the parent function
+    setTimeout(() => {
+      setIsSaved(false);
+    }, 1000);
+  };
+
   return (
-    <button className="save-btn" onClick={onClick}>
-      Save
+    <button className="save-btn" onClick={handleClick}>
+      {isSaved ? 'Saved ✨' : 'Save'}
     </button>
   );
 }
